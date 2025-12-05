65fb8c4f90e02d908de0d7e65b70e746215d8245
Treasure-king <107706054+Treasure-king@users.noreply.github.com>
2025-12-05 19:11:05 +0530

    chnage ssl false

--- digi-backend/src/config/db.js
+++ digi-backend/src/config/db.js
@@ -1,15 +1,15 @@
 const postgres = require("postgres");
 
-let sql; // shared db instance
+let sql;
 
 const connectDB = async () => {
   try {
     const connectionString = process.env.DATABASE_URL;
 
     sql = postgres(connectionString, {
-      ssl: {
-        rejectUnauthorized: false // important for Supabase on cloud
-      }
+      ssl: { rejectUnauthorized: false },
+      host: connectionString.split("@")[1].split(":")[0], // force host from connection string
+      family: 4 // IPv4
     });
 
     await sql`SELECT NOW()`;
@@ -18,7 +18,7 @@ const connectDB = async () => {
     return sql;
   } catch (err) {
     console.error("Database connection error:", err);
-    process.exit(1); // optional: stop server if DB fails
+    process.exit(1);
   }
 };
 
